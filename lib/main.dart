299c45eaea044a9d78b6a9ed75ef7973ec92848c
Samuel Lestar <85190240+samuellestar@users.noreply.github.com>
2022-05-15 11:32:34 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:grievance_portal/screens/screen_splash.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: ' Grievance Portal',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ScreenSplash(),
+    );
+  }
+}
