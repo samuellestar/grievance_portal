f168dc774d8583e0aa4120d97e452d91e68a61e3
Samuel Lestar <85190240+samuellestar@users.noreply.github.com>
2022-07-03 09:58:16 +0530

    Add files via upload

--- lib/Authentication/main_page.dart
+++ lib/Authentication/main_page.dart
@@ -1,1 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:grievance_user_portal/screens/screen_home.dart';
+import 'package:grievance_user_portal/screens/screen_login.dart';
 
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const ScreenHome();
+          } else {
+            return const ScreenLogin();
+          }
+        },
+      ),
+    );
+  }
+}
